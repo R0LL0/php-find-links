c6931a70ddf5c52e0323012766dd619c3646d8af
R0LL0 <67107891+R0LL0@users.noreply.github.com>
2021-08-29 23:54:19 +0300

    Create url-link-search.php

---
+++ url-link-search.php
@@ -0,0 +1,18 @@
+<?php
+
+function search($url){    
+    $content=file_get_contents($url);
+    $subString = preg_split("/<\/a>/",$content);
+    foreach ( $subString as $val ){
+        if( strpos($val, "<a href=") !== FALSE ){
+            $val = preg_replace("/.*<a\s+href=\"/sm","",$val);
+            $val = preg_replace("/\".*/","",$val);
+            echo $val . "\n";
+        
+        }
+    }    
+}
+
+search("input your URL here");
+
+?>
