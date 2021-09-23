5cb5a851bad32dd86ab36fa2c42abedf273c1d0d
R0LL0 <67107891+R0LL0@users.noreply.github.com>
2021-09-23 21:21:29 +0300

    Added Bootstrap CDN CSS and JS Scripts

--- realtime-progress-bar.php
+++ realtime-progress-bar.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <!-- Insert your style sheets and scripts here -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
         <div class="container">
@@ -35,6 +36,7 @@
                 document.getElementById('loadarea').src = '';
             });
         </script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
     </body>
 </html>
 
